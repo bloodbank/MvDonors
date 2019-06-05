d6e8489323811f872dbae8215483ecdf5a2777df
Ibrahim Hussain <ibrahim.hucyn@live.com>
2019-06-05 19:30:50 +0500

    Added Group reactions map

--- BloodBankData/dbo/Tables/BloodGroup.sql
+++ BloodBankData/dbo/Tables/BloodGroup.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[BloodGroup]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] TINYINT NOT NULL PRIMARY KEY, 
     [GroupRh] CHAR(10) NOT NULL, 
 	/*Maps Group to the it's respective grading.*/
 
---
+++ BloodBankData/dbo/Tables/BloodGroupReactionsMap.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[BloodGroupReactionsMap]
+(
+	[Id] TINYINT NOT NULL PRIMARY KEY, 
+    [A] BIT NOT NULL, 
+    [B] BIT NOT NULL, 
+    [AB] BIT NOT NULL, 
+    [D] BIT NOT NULL, 
+    [ACells] BIT NOT NULL, 
+    [BCells] BIT NOT NULL, 
+    [OCells] BIT NOT NULL, 
+    [Du] BIT NOT NULL
+)
