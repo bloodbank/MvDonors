dd3f68e4859420d58ee5f3b9dec592fe2587f05c
Ibrahim Hussain <ibrahim.hucyn@live.com>
2019-06-08 21:18:48 +0500

    REFACTORING

--- BloodBankData/dbo/Tables/Anticoagulant.sql
+++ BloodBankData/dbo/Tables/Anticoagulant.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Anticoagulant]
 (
 	[Id] TINYINT NOT NULL PRIMARY KEY, 
-    [Value] NCHAR(10) NULL, 
+    [Value] CHAR(10) NULL, 
     CONSTRAINT [AK_Anticoagulant_Value] UNIQUE ([Value])
 )
